c067eb9862e3d13a5ce68e48542210922e6b62ea
Thamaraiselvimurugan <40590355+Thamaraiselvimurugan@users.noreply.github.com>
2018-12-07 07:47:57 +0530

    Create ifelse.c

---
+++ ifelse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("Press a number : ");
+	scanf("%d",&n);
+	if(n!=0 && n<100)
+	{
+		if(n%2==0)
+		{
+		    printf("Doll will sing\n");
+		}
+	
+	else
+	printf("Invalid number\n");
+}
+
+}
+
